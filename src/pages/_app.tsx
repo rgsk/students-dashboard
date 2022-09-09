3bd720c72bc53ab927a02d6e71369cbc0d98c5b2
rgsk <guptarahul70322@gmail.com>
2022-09-10 02:44:16 +0530

    head update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import 'styles/globals.css';
 
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Students Dashboard</title>
+        <meta name="description" content="Welcome to Students Dashboard" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
