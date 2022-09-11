e688ed4c3b0db40e5d1b1d582303e617ef77c88a
rgsk <guptarahul70322@gmail.com>
2022-09-12 01:27:54 +0530

    dynamic import removed for AttendancesPage

--- src/pages/attendances/index.tsx
+++ src/pages/attendances/index.tsx
@@ -1,17 +1,8 @@
+import AttendancesPage from 'components/AttendancesPage/AttendancesPage';
 import AuthGuard from 'components/Global/AuthGuard';
 import Layout from 'components/Global/Layout/Layout';
 import { NextPage } from 'next';
-import dynamic from 'next/dynamic';
 
-// data getting populated by localStorage was
-// leading to rendering different stuff on server and client side
-// we did this to prevent error raised by nextjs
-const AttendancesPage = dynamic(
-  () => import('components/AttendancesPage/AttendancesPage'),
-  {
-    ssr: false,
-  }
-);
 const NextAttendancesPage: NextPage & {
   PageLayout: typeof Layout;
 } = ({}) => {
