30d619af10fdcc2e7bebbea49719569ce987521c
rgsk <guptarahul70322@gmail.com>
2022-09-12 01:54:26 +0530

    home page added

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+interface IHomePageProps {}
+const HomePage: React.FC<IHomePageProps> = ({}) => {
+  return (
+    <div>
+      <p>Hi, nothing here please visit one of above links</p>
+    </div>
+  );
+};
+export default HomePage;
